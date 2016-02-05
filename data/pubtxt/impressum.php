b0edd1fe88510c061f19dc3df71a9c2831cf5808
Andreas Grzywatz <dergriewatz@users.noreply.github.com>
2016-02-06 00:29:27 +0100

    auto reformat

--- data/pubtxt/impressum.php
+++ data/pubtxt/impressum.php
@@ -1,13 +1,14 @@
 <div class="content" id="public-doc-impressum">
 
-<h2>Impressum</h2>
+    <h2>Impressum</h2>
 
-<h3>Verantwortlicher</h3>
-<p>
-blub</p>
-<p>E-Mail: blub<br />
-Internet: blub</p>
+    <h3>Verantwortlicher</h3>
+    <p>
+        blub</p>
+    <p>E-Mail: blub<br/>
+        Internet: blub</p>
 
-<p>Haftungshinweis: Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.</p>
+    <p>Haftungshinweis: Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer
+        Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.</p>
 
 </div>
--- derefer.php
+++ derefer.php
@@ -1,10 +1,10 @@
 <?php
 
-$url=$_GET['u'];
-$url=urldecode($url);
+$url = $_GET['u'];
+$url = urldecode($url);
 
-if(eregi('http://.*',$url)!=false) {
-echo '<html>
+if (eregi('http://.*', $url) != false) {
+    echo '<html>
 <head>
 <meta http-equiv="REFRESH" content="0; URL='.$url.'">
 </head>
